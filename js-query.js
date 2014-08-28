1a4d9ec5a2c76d01109a3f7ef1313d4534268e77
Ramy Eldesoky <rdesoky@gmail.com>
2014-08-28 12:46:22 -0700

    first implementations

--- js-query.js
+++ js-query.js
@@ -2,5 +2,36 @@
  * Created by Ramy on 8/27/2014.
  */
 (function(){
-
+	var fn = {
+		html:function(shtml){
+			this.innerHTML = shtml;
+			return this;
+		},
+		ready:function(cb){
+			return this.listen("ready",cb);
+		},
+		listen:function(id, cb){
+			this.addEventListener(id, cb);
+			return this;
+		}
+	};
+	var utils = {
+		isArray:function(o){
+			return Array.isArray(0);
+		}
+	};
+	
+	var $ = function(q){
+		var el = document.querySelector(q);
+		el.prototype = fn;
+	};
+	
+	$.prototype = utils;
+	
+	if(require && require.amd){
+		define(function(){
+			return $;
+		});
+	}
+	
 })();
