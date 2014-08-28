2e3cfab4c945498dc3917f20ae27c443ac33ec1e
Ramy Eldesoky <rdesoky@gmail.com>
2014-08-28 14:33:32 -0700

    after initial sucessfull testing

--- js-query.js
+++ js-query.js
@@ -7,13 +7,36 @@
 			this.innerHTML = shtml;
 			return this;
 		},
+        text:function(txt){
+            this.innerText = txt;
+            return this;
+        },
 		ready:function(cb){
-			return this.listen("ready",cb);
+            if(this == document){
+                return $(window).ready(cb);
+            }
+			return this.listen("load",cb);
 		},
 		listen:function(id, cb){
 			this.addEventListener(id, cb);
 			return this;
-		}
+		},
+        click:function(cb){
+            return this.listen("click", cb);
+        },
+        show:function(){
+            return this.css("display","none");
+        },
+        hide:function(){
+            return this.css("display","");
+        },
+        css:function(s,val){
+            this.style[s]=val;
+            return this;
+        },
+        prepend:function(el){
+            this.insertBefore(el, this.firstChild);
+        }
 	};
 	var utils = {
 		isArray:function(o){
@@ -22,16 +45,31 @@
 	};
 	
 	var $ = function(q){
-		var el = document.querySelector(q);
-		el.prototype = fn;
+        var ret = q;
+        if(typeof q === "string"){
+            var tagMatch = /\<([A-z]+).*\>/;
+            var tags = tagMatch.exec(q)
+            if(tags){
+                ret = document.createElement(tags[1]);
+            }else {
+                ret = document.querySelector(q);
+            }
+        }
+        for(var f in fn){
+            ret[f] = fn[f];
+        }
+
+        return  ret;
 	};
 	
 	$.prototype = utils;
 	
-	if(require && require.amd){
+	if(typeof define === "function" && define.amd){
 		define(function(){
 			return $;
 		});
-	}
+	}else{
+        window.$ = $;
+    }
 	
 })();
